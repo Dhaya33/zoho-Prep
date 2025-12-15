fa8ba8a5e12c6095638eee60b4e03396296e0467
Dhaya33 <dhaya33projects@gmail.com>
2025-12-15 19:38:26 +0530

    check commit

--- pattern5.java
+++ pattern5.java
@@ -9,6 +9,7 @@ public class pattern5 {
         for (int row = 1; row <= 2*n-1; row++) {
 
             int totalCol= row > n ? 2*n-row : row;
+
             for (int col = 1; col <= totalCol; col++) {
 
                 System.out.print("* ");
